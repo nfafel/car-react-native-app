8e7d0f719fb87e837516d0a834a6536adf7da6e5
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-07-17 09:36:00 -0400

    fixed date picker issue

--- CarFormComponent.js
+++ CarFormComponent.js
@@ -96,80 +96,82 @@ class CarFormComponent extends Component {
     render() {
         return (
             <Modal 
+                avoidKeyboard={true}
                 style={{
-                    backgroundColor: 'white',
                     margin: 20,
                     flex: 1,
                 }}
                 isVisible={this.props.shouldGetPutData || this.props.shouldGetPostData}>
-                <Row textStyle={{textAlign: 'center'}} style={{flex:1,}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Year</Text>
-                        {this.props.formikProps.errors.year &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.year}</Text>
-                        } 
-                    </View>, 
-                    <Picker key="yearForm"
-                        itemStyle={{height: 140}}
-                        onValueChange={this.props.formikProps.handleChange('year')}
-                        selectedValue={this.props.formikProps.values.year}
-                    >
-                        {this.getYearOptions()}
-                    </Picker> ]} 
-                />
-                <Row textStyle={{textAlign: 'center'}} style={{flex:1}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Make</Text>
-                        {this.props.formikProps.errors.make &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.make}</Text>
-                        } 
-                    </View>,
-                    <Picker key="makeForm"
-                        itemStyle={{height: 140}}
-                        onValueChange={this.props.formikProps.handleChange('make')}
-                        selectedValue={this.props.formikProps.values.make}
-                    >
-                        {this.getMakeOptions(this.props.formikProps.values, this.props.formikProps.setFieldValue)}
-                    </Picker> ]} 
-                />
-                <Row textStyle={{textAlign: 'center'}} style={{flex:1}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Model</Text>
-                        {this.props.formikProps.errors.model &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.model}</Text>
-                        } 
-                    </View>,
-                    <Picker key="modelForm"
-                        itemStyle={{height: 140}}
-                        onValueChange={this.props.formikProps.handleChange('model')}
-                        selectedValue={this.props.formikProps.values.model}
-                    >
-                        {this.getModelOptions(this.props.formikProps.values, this.props.formikProps.setFieldValue)}
-                    </Picker> ]} 
-                />
-                <Row textStyle={{textAlign: 'center'}} style={{flex:1}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Rating</Text>
-                        {this.props.formikProps.errors.rating &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.rating}</Text>
-                        } 
-                    </View>,
-                    <TextInput key="ratingForm"
-                        onChangeText={this.props.formikProps.handleChange('rating')}
-                        value={""+this.props.formikProps.values.rating}
-                        placeholder="Rating"
-                        keyboardType='numeric'
-                    /> ]} 
-                />
-                <Row data={[
-                    <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.cancel}>
-                        <Text style={{color: 'white', fontSize: 19}}>CANCEL</Text>
-                    </TouchableOpacity>,
-                    <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.formikProps.handleSubmit} >
-                        <Text style={{color: 'white', fontSize: 19}}>{this.props.buttonText}</Text>
-                    </TouchableOpacity>
-                    ]}
-                />
+                <View style={{flex:1, backgroundColor: 'white'}}>
+                    <Row textStyle={{textAlign: 'center'}} style={{flex:1,}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Year</Text>
+                            {this.props.formikProps.errors.year &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.year}</Text>
+                            } 
+                        </View>, 
+                        <Picker key="yearForm"
+                            itemStyle={{height: 140}}
+                            onValueChange={this.props.formikProps.handleChange('year')}
+                            selectedValue={this.props.formikProps.values.year}
+                        >
+                            {this.getYearOptions()}
+                        </Picker> ]} 
+                    />
+                    <Row textStyle={{textAlign: 'center'}} style={{flex:1}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Make</Text>
+                            {this.props.formikProps.errors.make &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.make}</Text>
+                            } 
+                        </View>,
+                        <Picker key="makeForm"
+                            itemStyle={{height: 140}}
+                            onValueChange={this.props.formikProps.handleChange('make')}
+                            selectedValue={this.props.formikProps.values.make}
+                        >
+                            {this.getMakeOptions(this.props.formikProps.values, this.props.formikProps.setFieldValue)}
+                        </Picker> ]} 
+                    />
+                    <Row textStyle={{textAlign: 'center'}} style={{flex:1}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Model</Text>
+                            {this.props.formikProps.errors.model &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.model}</Text>
+                            } 
+                        </View>,
+                        <Picker key="modelForm"
+                            itemStyle={{height: 140}}
+                            onValueChange={this.props.formikProps.handleChange('model')}
+                            selectedValue={this.props.formikProps.values.model}
+                        >
+                            {this.getModelOptions(this.props.formikProps.values, this.props.formikProps.setFieldValue)}
+                        </Picker> ]} 
+                    />
+                    <Row textStyle={{textAlign: 'center'}} style={{flex:1}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Rating</Text>
+                            {this.props.formikProps.errors.rating &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.rating}</Text>
+                            } 
+                        </View>,
+                        <TextInput key="ratingForm"
+                            onChangeText={this.props.formikProps.handleChange('rating')}
+                            value={""+this.props.formikProps.values.rating}
+                            placeholder="Rating"
+                            keyboardType='numeric'
+                        /> ]} 
+                    />
+                    <Row data={[
+                        <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.cancel}>
+                            <Text style={{color: 'white', fontSize: 19}}>CANCEL</Text>
+                        </TouchableOpacity>,
+                        <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.formikProps.handleSubmit} >
+                            <Text style={{color: 'white', fontSize: 19}}>{this.props.buttonText}</Text>
+                        </TouchableOpacity>
+                        ]}
+                    />
+                </View>
             </Modal>
         )  
     }
--- RepairFormComponent.js
+++ RepairFormComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { View, Text, Button, TextInput, Picker, TouchableOpacity } from 'react-native';
 import { Row, Col, Table } from 'react-native-table-component';
-import DatePicker from 'react-native-datepicker';
+import DateTimePicker from "react-native-modal-datetime-picker";
 import Modal from "react-native-modal";
 
 class RepairFormComponent extends Component {
@@ -35,126 +35,126 @@ class RepairFormComponent extends Component {
 
     getInitDate = () => {
         if (this.props.formikProps.values.date === "") {
-            return;
+            return (new Date());
         } else if (this.props.formikProps.values.date instanceof Date) {
             return (this.props.formikProps.values.date);
-        }
+        } 
         return (new Date(this.props.formikProps.values.date));
     }
 
     repairForm = () => {
         return ( 
             <Modal 
+                avoidKeyboard={true}
                 style={{
-                    backgroundColor: 'white',
                     margin: 20,
                     flex: 1,
                 }}
                 isVisible={this.props.visible}>
-                <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Car</Text>
-                        {this.props.formikProps.errors.car_id &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.car_id}</Text>
-                        } 
-                    </View>,
-                    <Picker 
-                        itemStyle={{height: 100}}
-                        onValueChange={this.props.formikProps.handleChange('car_id')}
-                        selectedValue={this.props.formikProps.values.car_id}
-                    >
-                        {this.carOptions()}
-                    </Picker> ]} 
-                />
-                <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Date Admitted</Text>
-                        {this.props.formikProps.errors.date &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.date}</Text>
-                        } 
-                    </View>, 
-                    <View>
-                        <DatePicker
-                            style={{alignSelf: 'center'}}
-                            mode='date'
-                            date={this.getInitDate()}
-                            confirmBtnText="Confirm"
-                            cancelBtnText="Cancel"
-                            onDateChange={this.props.formikProps.handleChange('date')}
-                            //onConfirm={() => this.setState({datePickerVisible: false})}
-                            //isVisible={this.state.datePickerVisible}
-                            //onCancel={() => this.setState({datePickerVisible: false})}
-                        />
-                        
-                    </View> ]} 
-                />
-                <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Description</Text>
-                        {this.props.formikProps.errors.description &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.description}</Text>
-                        } 
-                    </View>,
-                    <TextInput
-                        onChangeText={this.props.formikProps.handleChange('description')}
-                        value={""+this.props.formikProps.values.description}
-                        placeholder="Description"
-                    /> ]} 
-                />
-                <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[ 
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Cost</Text>
-                        {this.props.formikProps.errors.cost &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.cost}</Text>
-                        } 
-                    </View>,
-                    <TextInput
-                        onChangeText={this.props.formikProps.handleChange('cost')}
-                        value={""+this.props.formikProps.values.cost}
-                        placeholder="Cost"
-                        keyboardType='numeric'
-                    /> ]} 
-                />
-                <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Progress</Text>
-                        {this.props.formikProps.errors.progress &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.progress}</Text>
-                        } 
-                    </View>,
-                    <Picker 
-                        itemStyle={{height: 100}}
-                        onValueChange={this.props.formikProps.handleChange('progress')}
-                        selectedValue={this.props.formikProps.values.progress}
-                    >
-                        <Picker.Item value="" label="Select Progress" />
-                        <Picker.Item value="Ready" label="Ready" />
-                        <Picker.Item value="In Progress" label="In Progress" />
-                        <Picker.Item value="Completed" label="Completed" />
-                    </Picker> ]} 
-                />
-                <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
-                    <View>
-                        <Text style={{ fontSize: 20, alignSelf: 'center' }}>Technician</Text>
-                        {this.props.formikProps.errors.technician &&
-                            <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.technician}</Text>
-                        } 
-                    </View>,
-                    <TextInput
-                        onChangeText={this.props.formikProps.handleChange('technician')}
-                        value={""+this.props.formikProps.values.technician}
-                        placeholder="Technician"
-                    /> ]} 
-                />
-                <Row textStyle={{textAlign: 'center'}} data={[
-                    <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.cancel} >
-                        <Text style={{color: 'white', fontSize: 18}}>CANCEL</Text>
-                    </TouchableOpacity>,
-                    <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.formikProps.handleSubmit} >
-                        <Text style={{color: 'white', fontSize: 18}}>{this.props.submitText}</Text>
-                    </TouchableOpacity>
-                    ]}
-                />
+                <View style={{flex:1, backgroundColor: 'white'}}>
+                    <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Car</Text>
+                            {this.props.formikProps.errors.car_id &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.car_id}</Text>
+                            } 
+                        </View>,
+                        <Picker 
+                            itemStyle={{height: 100}}
+                            onValueChange={this.props.formikProps.handleChange('car_id')}
+                            selectedValue={this.props.formikProps.values.car_id}
+                        >
+                            {this.carOptions()}
+                        </Picker> ]} 
+                    />
+                    <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Date Admitted</Text>
+                            {this.props.formikProps.errors.date &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.date}</Text>
+                            } 
+                        </View>, 
+                        <View>
+                            <DateTimePicker
+                                date={this.getInitDate()}
+                                onConfirm={(date) => {this.props.formikProps.setFieldValue('date', date ); this.setState({datePickerVisible: false})}}
+                                isVisible={this.state.datePickerVisible}
+                                onCancel={() => this.setState({datePickerVisible: false})}
+                                onHideAfterConfirm={() => this.setState({datePickerVisible: false})}
+                            />
+                            <TouchableOpacity style={{backgroundColor: 'white', justifyContent: 'center', flexDirection: 'row'}} onPress={() => this.setState({datePickerVisible: true})} >
+                                <Text style={{color: 'black', fontSize: 16}}>{this.getDateTitle()}</Text>
+                            </TouchableOpacity>
+                        </View> ]} 
+                    />
+                    <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Description</Text>
+                            {this.props.formikProps.errors.description &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.description}</Text>
+                            } 
+                        </View>,
+                        <TextInput
+                            onChangeText={this.props.formikProps.handleChange('description')}
+                            value={""+this.props.formikProps.values.description}
+                            placeholder="Description"
+                        /> ]} 
+                    />
+                    <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[ 
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Cost</Text>
+                            {this.props.formikProps.errors.cost &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.cost}</Text>
+                            } 
+                        </View>,
+                        <TextInput
+                            onChangeText={this.props.formikProps.handleChange('cost')}
+                            value={""+this.props.formikProps.values.cost}
+                            placeholder="Cost"
+                            keyboardType='numeric'
+                        /> ]} 
+                    />
+                    <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Progress</Text>
+                            {this.props.formikProps.errors.progress &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.progress}</Text>
+                            } 
+                        </View>,
+                        <Picker 
+                            itemStyle={{height: 100}}
+                            onValueChange={this.props.formikProps.handleChange('progress')}
+                            selectedValue={this.props.formikProps.values.progress}
+                        >
+                            <Picker.Item value="" label="Select Progress" />
+                            <Picker.Item value="Ready" label="Ready" />
+                            <Picker.Item value="In Progress" label="In Progress" />
+                            <Picker.Item value="Completed" label="Completed" />
+                        </Picker> ]} 
+                    />
+                    <Row style={{flex:1}} textStyle={{textAlign: 'center'}} data={[
+                        <View>
+                            <Text style={{ fontSize: 20, alignSelf: 'center' }}>Technician</Text>
+                            {this.props.formikProps.errors.technician &&
+                                <Text style={{ fontSize: 15, color: 'red', alignSelf: 'center' }}>{this.props.formikProps.errors.technician}</Text>
+                            } 
+                        </View>,
+                        <TextInput
+                            onChangeText={this.props.formikProps.handleChange('technician')}
+                            value={""+this.props.formikProps.values.technician}
+                            placeholder="Technician"
+                        /> ]} 
+                    />
+                    <Row textStyle={{textAlign: 'center'}} data={[
+                        <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.cancel} >
+                            <Text style={{color: 'white', fontSize: 18}}>CANCEL</Text>
+                        </TouchableOpacity>,
+                        <TouchableOpacity style={{backgroundColor: '#57b0ff', justifyContent: 'center', flexDirection: 'row'}} onPress={this.props.formikProps.handleSubmit} >
+                            <Text style={{color: 'white', fontSize: 18}}>{this.props.submitText}</Text>
+                        </TouchableOpacity>
+                        ]}
+                    />
+                </View>
             </Modal>
         )
     }
